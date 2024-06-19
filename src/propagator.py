c1b7926895dd5dc1a0215ce08a3854e6c94d2df4
Jonathan Dong <jonathan.dong@epfl.ch>
2024-06-19 16:22:58 +0200

    Add refractive index of immersion medium

--- src/propagator.py
+++ src/propagator.py
@@ -9,8 +9,9 @@ from torch.special import bessel_j0
 
 class Propagator(ABC):
     def __init__(self, pupil, n_pix_psf=128, device='cpu',
-                 wavelength=632, NA=0.9, fov=2000, 
-                 defocus_min=0, defocus_max=0, n_defocus=1):
+                 wavelength=632, NA=0.9, fov=2000, refractive_index=1.5, 
+                 defocus_min=0, defocus_max=0, n_defocus=1,
+                 apod_factor=False, envelope=None):
         self.pupil = pupil
         
         self.n_pix_psf = n_pix_psf
@@ -27,9 +28,14 @@ class Propagator(ABC):
         self.wavelength = wavelength
         self.NA = NA
         self.fov = fov
+        self.refractive_index = refractive_index
+
         self.defocus_min = defocus_min
         self.defocus_max = defocus_max
         self.n_defocus = n_defocus
+
+        self.apod_factor = apod_factor
+        self.envelope = envelope
 
         self.field = None
 
@@ -40,24 +46,25 @@ class Propagator(ABC):
     
 class ScalarCartesianPropagator(Propagator):
     def __init__(self, pupil, n_pix_psf=128, device='cpu',
-                 wavelength=632, NA=0.9, fov=1000, 
+                 wavelength=632, NA=0.9, fov=1000, refractive_index=1.5, 
                  defocus_min=0, defocus_max=0, n_defocus=1,
                  sz_correction=True, apod_factor=False, envelope=None):
         super().__init__(pupil=pupil, n_pix_psf=n_pix_psf, device=device,
-                         wavelength=wavelength, NA=NA, fov=fov, 
-                         defocus_min=defocus_min, defocus_max=defocus_max, n_defocus=n_defocus)
+                         wavelength=wavelength, NA=NA, fov=fov, refractive_index=refractive_index, 
+                         defocus_min=defocus_min, defocus_max=defocus_max, n_defocus=n_defocus,
+                         apod_factor=apod_factor, envelope=envelope)
         self.sz_correction = sz_correction
-        self.apod_factor = apod_factor  # we may want to add more options, sometimes we need to divide by sqrt(s_z)
-        self.envelope = envelope  # for Gaussian envelope
         
          # Zoom factor to determine pixel size with custom FFT
-        self.zoom_factor = 2 * self.NA * self.fov / self.wavelength / self.n_pix_pupil
+        self.zoom_factor = 2 * self.NA * self.fov / self.wavelength \
+             / self.refractive_index / self.n_pix_pupil
 
         # Compute coordinates s_x, s_y, s_z
         n_pix_pupil = self.pupil.n_pix_pupil
         x = torch.linspace(-1, 1, n_pix_pupil).to(self.device)
         s_x, s_y = torch.meshgrid(x, x, indexing='ij')
-        s_z = torch.sqrt((1 - self.NA**2 * (s_x**2 + s_y**2)).clamp(min=0.001)).reshape(1, 1, n_pix_pupil, n_pix_pupil)
+        s_z = torch.sqrt((1 - (self.NA/self.refractive_index)**2 * (s_x**2 + s_y**2)
+                          ).clamp(min=0.001)).reshape(1, 1, n_pix_pupil, n_pix_pupil)
 
         # Precompute additional factors
         self.correction_factor = torch.ones(1, 1, n_pix_pupil, n_pix_pupil).to(self.device)
@@ -78,20 +85,19 @@ class ScalarCartesianPropagator(Propagator):
                                   k_start=-self.zoom_factor*np.pi, 
                                   k_end=self.zoom_factor*np.pi, 
                                   norm='backward', fftshift_input=True, include_end=True) * \
-                                    (2 * self.NA / self.n_pix_pupil)**2
-        return self.field / (2 * np.pi)
+                                    (2 * self.NA / self.refractive_index / self.n_pix_pupil)**2
+        return self.field / (2 * np.pi) / np.sqrt(self.refractive_index)
 
 
 class ScalarPolarPropagator(Propagator):
     def __init__(self, pupil, n_pix_psf=128, device='cpu',
-                 wavelength=632, NA=0.9, fov=1000, 
+                 wavelength=632, NA=0.9, fov=1000, refractive_index=1.5,
                  defocus_min=0, defocus_max=0, n_defocus=1,
                  apod_factor=False, envelope=None):
         super().__init__(pupil=pupil, n_pix_psf=n_pix_psf, device=device,
-                         wavelength=wavelength, NA=NA, fov=fov, 
-                         defocus_min=defocus_min, defocus_max=defocus_max, n_defocus=n_defocus)
-        self.apod_factor = apod_factor
-        self.envelope = envelope
+                         wavelength=wavelength, NA=NA, fov=fov, refractive_index=refractive_index,
+                         defocus_min=defocus_min, defocus_max=defocus_max, n_defocus=n_defocus, 
+                         apod_factor=apod_factor, envelope=envelope)
         
         # PSF coordinates
         x = torch.linspace(-self.fov/2, self.fov/2, self.n_pix_psf)
@@ -99,7 +105,7 @@ class ScalarPolarPropagator(Propagator):
         self.r = torch.sqrt(xx ** 2 + yy ** 2).unsqueeze(0).unsqueeze(0).unsqueeze(-1).to(self.device)
 
         # Pupil coordinates
-        self.theta_max = np.arcsin(self.NA)
+        self.theta_max = np.arcsin(self.NA / self.refractive_index)
         theta = torch.linspace(0, self.theta_max, self.n_pix_pupil).to(self.device)
 
         # Precompute additional factors
@@ -121,7 +127,7 @@ class ScalarPolarPropagator(Propagator):
             self.pupil.field.unsqueeze(-2).unsqueeze(-2) *
             bessel_j0(self.k * self.r * self.sin_t) *
             self.sin_t * self.defocus_filters * self.correction_factor
-            , dim=-1) * self.theta_max / self.n_pix_pupil
+            , dim=-1) * self.theta_max / self.n_pix_pupil / np.sqrt(self.refractive_index)
         return self.field
 
 
