Fix sign error in vectorial Ez

src/propagator.py
@@ -357,7 +357,7 @@ class VectorialPolarPropagator(Propagator):
         PSF_field = torch.stack([                                   # [n_channels=3, size_x, size_y]
             Ix0 - Ix2 * self.cos_twophi - Iy2 * self.sin_twophi,
             Iy0 - Ix2 * self.sin_twophi + Iy2 * self.cos_twophi,
-            2.0 * (Ix1 * self.cos_phi + Iy1 * self.sin_phi)],
+            -2j * (Ix1 * self.cos_phi + Iy1 * self.sin_phi)],
             dim=0)
         
         # TODO: why divide by an extra factor of sqrt(eta)?

src/testers.py
@@ -303,9 +303,9 @@ class VectorPupilCase(TestCase):
 
         # updated expression with correct 1j factors
         psf_field = torch.stack(
-            [I0x + (-1.0) * I2x * cos_twophi + (-1.0) * I2y * sin_twophi,
-             I0y + (-1.0) * I2x * sin_twophi - (-1.0) * I2y * cos_twophi,
-             -2j * 1j * (I1x * cos_phi + I1y * sin_phi)]
+            [I0x - I2x * cos_twophi - I2y * sin_twophi,
+             I0y - I2x * sin_twophi + I2y * cos_twophi,
+             -2j * (I1x * cos_phi + I1y * sin_phi)]
         )
         
         return pupil_field, psf_field