ea2b7a1378620720a573f392d158d0ad5e8668af
Yan Liu <yan.liu@epfl.ch>
2024-08-29 15:24:28 +0200

    revert the movement of the computation of attributes back to the constructor

--- src/propagators/vectorial_polar_propagator.py
+++ src/propagators/vectorial_polar_propagator.py
@@ -10,6 +10,33 @@ from .polar_propagator import PolarPropagator
 
 
 class VectorialPolarPropagator(VectorialPropagator, PolarPropagator):
+    def __init__(self, pupil, n_pix_psf=128, device='cpu',
+                 wavelength=632, na=1.3, fov=1000, refractive_index=1.5,
+                 defocus_min=0, defocus_max=0, n_defocus=1,
+                 apod_factor=False, envelope=None, cos_factor=False,
+                 gibson_lanni=False, z_p=1e3, n_s=1.3,
+                 n_g=1.5, n_g0=1.5, t_g=170e3, t_g0=170e3,
+                 n_i=1.5, t_i0=100e3,
+                 quadrature_rule=simpsons_rule):
+        super().__init__(pupil=pupil, n_pix_psf=n_pix_psf, device=device,
+                         wavelength=wavelength, na=na, fov=fov, refractive_index=refractive_index,
+                         defocus_min=defocus_min, defocus_max=defocus_max, n_defocus=n_defocus,
+                         apod_factor=apod_factor, envelope=envelope, cos_factor=cos_factor,
+                         gibson_lanni=gibson_lanni, z_p=z_p, n_s=n_s,
+                         n_g=n_g, n_g0=n_g0, t_g=t_g, t_g0=t_g0,
+                         n_i=n_i, t_i0=t_i0,
+                         quadrature_rule=quadrature_rule)
+
+        # PSF varphi coordinate
+        varphi = torch.atan2(self.yy, self.xx)
+        sin_phi = torch.sin(varphi)
+        cos_phi = torch.cos(varphi)
+        sin_twophi = 2.0 * sin_phi * cos_phi
+        cos_twophi = cos_phi ** 2 - sin_phi ** 2
+        self.sin_phi = sin_phi.to(self.device)
+        self.cos_phi = cos_phi.to(self.device)
+        self.sin_twophi = sin_twophi.to(self.device)
+        self.cos_twophi = cos_twophi.to(self.device)
 
     def _get_input_field(self) -> torch.Tensor:
         """Get the input field for vectorial polar propagator."""
@@ -71,17 +98,6 @@ class VectorialPolarPropagator(VectorialPropagator, PolarPropagator):
         Iy1 = Iy1[self.rr_indices]
         Ix2 = Ix2[self.rr_indices]
         Iy2 = Iy2[self.rr_indices]
-
-        # PSF varphi coordinate
-        varphi = torch.atan2(self.yy, self.xx)
-        sin_phi = torch.sin(varphi)
-        cos_phi = torch.cos(varphi)
-        sin_twophi = 2.0 * sin_phi * cos_phi
-        cos_twophi = cos_phi ** 2 - sin_phi ** 2
-        self.sin_phi = sin_phi.to(self.device)
-        self.cos_phi = cos_phi.to(self.device)
-        self.sin_twophi = sin_twophi.to(self.device)
-        self.cos_twophi = cos_twophi.to(self.device)
 
         # updated expression with correct 1j factors
         PSF_field = torch.stack([  # [n_channels=3, size_x, size_y]
