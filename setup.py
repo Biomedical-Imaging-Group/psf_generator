4b43cc3d5147321543569175b8879cd5fc7db636
VStergiop <stergiopoulouvasilina@gmail.com>
2024-10-16 19:48:45 +0200

    correct the setup.py

--- setup.py
+++ setup.py
@@ -4,28 +4,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PSF Generator",
+    name="propagators",
     version="0.0.1",
     author="",
     author_email="",
     description="PSF Generator.",
     long_description=long_description,
     url=" ",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},  # Tell setuptools that packages are under 'src'
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
     install_requires=[
-        # "torch",
-        # "abc",
-        # "math",
-        # "numpy",
-        # "scipy",
-        # "matplotlib",
-        # "zernikepy",
-        # "tqdm",
-    ],
-    include_package_data=True,
+        "matplotlib", "numpy", "scipy", "torch", "tqdm", "zernikepy",],
+    python_requires=">=3.8",
 )
