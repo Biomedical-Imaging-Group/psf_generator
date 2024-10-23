78204e3313c6ac8802b50bebd4189e7d2a870ea7
Yan Liu <yan.liu@epfl.ch>
2024-10-23 21:27:21 +0200

    use kwargs for the input, change order of for-loops, make code flexible when cuda is not available

--- demos/benchmark_runtime_scalar.py
+++ demos/benchmark_runtime_scalar.py
@@ -21,16 +21,16 @@ from tqdm import tqdm
 from propagators import ScalarCartesianPropagator, ScalarSphericalPropagator
 
 if __name__ == "__main__":
-    if not torch.cuda.is_available():
-        raise Exception('Cuda Backend not found')
-
     # psf parameters
-    n_pix_psf = 201
-    na = 0.9
-    wavelength = 632
-    fov = 3000
-    defocus = 0
-    n_defocus = 1
+    kwargs = {
+        'n_pix_psf': 201,
+        'wavelength': 632,
+        'na': 0.9,
+        'fov': 3000,
+        'defocus_min': 0,
+        'defocus_max': 0,
+        'n_defocus': 1,
+    }
     # define propagators
     propagators = [
         ScalarCartesianPropagator,
@@ -42,21 +42,23 @@ if __name__ == "__main__":
     number_of_repetitions = 10
     devices = ["cpu", "cuda:0"]
     # file path to save statistics
-    path = os.path.join('Results', 'data')
+    path = os.path.join('results', 'data')
 
-    for  n_pix_pupil in tqdm(list_of_pupil_pixels):
-        for propagator in propagators:
-            for device in devices:
-                if 'cuda' in device:
+    for propagator in propagators:
+        for device in devices:
+            if 'cuda' in device:
+                if torch.cuda.is_available():
                     torch.cuda.synchronize()
+                else:
+                    continue
+            average_runtime_list = []
+            for n_pix_pupil in tqdm(list_of_pupil_pixels):
                 runtime_list =[]
-                average_runtime_list = []
                 for _ in range(number_of_repetitions):
                     start_time = time()
-                    propagator(n_pix_pupil=n_pix_pupil, n_pix_psf=n_pix_psf, wavelength=wavelength, na=na, fov=fov,
-                               defocus_min=0, defocus_max=defocus, n_defocus=n_defocus, device=device).compute_focus_field()
+                    propagator(n_pix_pupil=n_pix_pupil, device=device, **kwargs).compute_focus_field()
                     runtime = time() - start_time
                     runtime_list.append(runtime)
                 average_runtime_list.append((n_pix_pupil, sum(runtime_list) / number_of_repetitions))
-                filename = f'{propagator}' + '_' + f'{device}'
-                save_stats(average_runtime_list, filename, path)
+            filename = f'{propagator.__name__.lower()}' + '_' + f'{device}'
+            save_stats(average_runtime_list, filename, path)
