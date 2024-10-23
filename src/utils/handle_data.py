43e213f1dd5b178882d065ef9d582048a517197f
Yan Liu <yan.liu@epfl.ch>
2024-10-23 17:05:48 +0200

    add save stats

--- src/utils/handle_data.py
+++ src/utils/handle_data.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import typing as tp
 
@@ -44,3 +45,29 @@ def save_image(image: tp.Union[torch.Tensor, np.ndarray], filename: str, path: s
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
     skio.imsave(filepath, image, check_contrast=False)
 
+
+def save_stats(data: list, filename: str, path: str):
+    """
+    Save statistical data, e.g. runtime values, in a systematic way for further analysis or plotting.
+
+    Statistical data such as the runtime values saved as a list of tuples (number of iteration, value) are written
+    as a csv file.
+    They can be used for plotting later to see the evolution of statistics.
+
+    Parameters
+    ----------
+    data : list
+        Statistics to be saved.
+    filename : str
+        Name of the file to store the statistics.
+    path : str
+        Path on which the file should be saved
+
+    """
+    filetype = '.csv'
+    filepath = os.path.join(path, filename + filetype)
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for row in data:
+            writer.writerow(row)
