2bdfcbe7e611ef7bc385d7814a93762f56babfe0
Yan Liu <yan.liu@epfl.ch>
2024-10-24 21:33:13 +0200

    rename file and add test pupil

--- tests/test_pupil.py
+++ tests/test_pupil.py
@@ -7,8 +7,10 @@ from propagators import *
 from utils.handle_data import save_as_npy, load_from_npy
 from utils.misc import convert_tensor_to_array
 
+N_PIX_PUPIL = 127
+
 kwargs = {
-            'n_pix_pupil': 127,
+            'n_pix_pupil': N_PIX_PUPIL,
             'n_pix_psf': 256,
             'wavelength': 632,
             'na': 1.4,
@@ -20,18 +22,17 @@ kwargs = {
             'gibson_lanni': False
         }
 
-@pytest.mark.parametrize('propagator_type', [
-    ScalarCartesianPropagator,
-    ScalarSphericalPropagator,
-    VectorialCartesianPropagator,
-    VectorialSphericalPropagator
+@pytest.mark.parametrize('propagator_type, pupil_shape', [
+    (ScalarCartesianPropagator, (N_PIX_PUPIL, N_PIX_PUPIL)),
+    (ScalarSphericalPropagator, (N_PIX_PUPIL,)),
+    (VectorialCartesianPropagator, (3, N_PIX_PUPIL, N_PIX_PUPIL)),
+    (VectorialSphericalPropagator, (2, N_PIX_PUPIL))
 ])
-def test_psf(propagator_type):
+def test_pupil(propagator_type, pupil_shape):
     """
-    Test if the PSF remains the same as the base one.
+    Test if the pupil remains the same as the base one.
 
     This is useful when changes are made to the propagators.
-    Before making changes, uncomment the line to save data, then comment it out to test.
 
     """
     is_vectorial = propagator_type in (VectorialCartesianPropagator, VectorialSphericalPropagator)
@@ -39,15 +40,12 @@ def test_psf(propagator_type):
         kwargs.update({'e0x': 1.0, 'e0y': 0.0})
 
     propagator = propagator_type(**kwargs)
-    n_channels = 3 if is_vectorial else 1
-    psf = propagator.compute_focus_field()
-    # check size of psfs
-    assert psf.shape == (kwargs['n_defocus'], n_channels, kwargs['n_pix_psf'], kwargs['n_pix_psf'])
+    pupil = propagator.get_input_field()
+    assert tuple(pupil.shape) == pupil_shape
 
-    filepath = os.path.join('results', 'data', f'{propagator.get_name()}_psf_base.npy')
+    filepath = os.path.join('results', 'data', f'{propagator.get_name()}_pupil_base.npy')
     # only save once and comment out for tests
-    # save_as_npy(filepath, psf)
-    base_psf = load_from_npy(filepath)
+    base_pupil = load_from_npy(filepath)
     # check content of PSFs
-    np.testing.assert_allclose(base_psf, convert_tensor_to_array(psf))
+    np.testing.assert_allclose(base_pupil, convert_tensor_to_array(pupil))
 
