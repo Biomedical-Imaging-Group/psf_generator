1ab20d0fd543ada5ef6cb127ab380a854ca42846
Yan Liu <yan.liu@epfl.ch>
2024-11-02 21:58:47 +0100

    expand benchmark on pupil to psf too

--- demos/benchmark_runtime.py
+++ demos/benchmark_runtime.py
@@ -1,7 +1,7 @@
 """
-Benchmark runtime of the two scalar propagators on CPU and GPU for a range of number of pixels of the pupil.
+Benchmark runtime of the two scalar propagators on CPU and GPU for a range of number of pixels of the pupil or PSF.
 
-The number of pixels of the PSF is fixed and only the one slice (focal plane) is computed.
+The other parameters are fixed and only the one slice (focal plane) is computed.
 
 """
 import math
@@ -20,27 +20,30 @@ from src.psf_generator.propagators import *
 from src.psf_generator.utils.handle_data import save_stats_as_csv
 
 
-def benchmark_runtime_pupil_size():
-    """Benchmark the runtime against the size of the pupil."""
+def benchmark_runtime_on_size(
+        quantity: str,
+        n_pix_pupil: int = 127,
+        n_pix_psf: int = 201,
+        wavelength: float = 632,
+        na: float = 0.9,
+        fov: int = 3000
+):
+    """Benchmark the runtime against the size of the pupil or PSF."""
     # propagator parameters
     kwargs = {
-        'n_pix_psf': 201,
-        'wavelength': 632,
-        'na': 0.9,
-        'fov': 3000,
-        'defocus_min': 0,
-        'defocus_max': 0,
-        'n_defocus': 1,
+        'wavelength': wavelength,
+        'na': na,
+        'fov': fov
     }
     # define propagators
-    propagators = [
+    propagator_types = [
         ScalarCartesianPropagator,
         ScalarSphericalPropagator,
         VectorialCartesianPropagator,
         VectorialSphericalPropagator,
     ]
     # test parameters
-    list_of_pupil_pixels = [int(math.pow(2, exponent)) for exponent in range(5, 13)]
+    list_of_pixels = [int(math.pow(2, exponent)) for exponent in range(5, 13)]
     number_of_repetitions = 10
     devices = ["cpu", "cuda:0"]
     # file path to save statistics
@@ -52,24 +55,30 @@ def benchmark_runtime_pupil_size():
                 torch.cuda.synchronize()
             else:
                 continue
-        for propagator in propagators:
+        for propagator_type in propagator_types:
             average_runtime_list = []
-            for n_pix_pupil in list_of_pupil_pixels:
-                print(device, propagator.__name__, n_pix_pupil)
+            for n_pix in list_of_pixels:
+                print(device, propagator_type.__name__, n_pix)
                 runtime_list = []
                 for _ in range(number_of_repetitions):
                     start_time = time()
-                    propagator(n_pix_pupil=n_pix_pupil, device=device, **kwargs).compute_focus_field()
+                    if quantity == 'pupil':
+                        propagator = propagator_type(n_pix_pupil=n_pix, n_pix_psf=n_pix_psf, device=device, **kwargs)
+                    elif quantity == 'psf':
+                        propagator = propagator_type(n_pix_pupil=n_pix_pupil, n_pix_psf=n_pix, device=device, **kwargs)
+                    else:
+                        raise ValueError(f"Unknown quantity {quantity}, choose 'pupil' or 'psf'")
+                    propagator.compute_focus_field()
                     runtime = time() - start_time
                     runtime_list.append(runtime)
                 average_runtime_list.append((n_pix_pupil, sum(runtime_list) / number_of_repetitions))
             # save stats
 
             device_name = 'gpu' if 'cuda' in device else 'cpu'
-            filename = f'{propagator.get_name()}_{device_name}'
+            filename = f'{propagator_type.get_name()}_{device_name}_{quantity}'
             filepath = os.path.join(path, filename + '.csv')
             save_stats_as_csv(filepath, average_runtime_list)
 
 
 if __name__ == "__main__":
-    benchmark_runtime_pupil_size()
+    benchmark_runtime_on_size('psf')
