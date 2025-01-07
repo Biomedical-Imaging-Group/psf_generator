f4d0b2f4ab54b9d3d35d361b94064700973785f6
Yan Liu <yan.liu@epfl.ch>
2025-01-07 15:53:44 +0100

    update parameters, remove comments, save pupil

--- demos/compute_quantities_paper.py
+++ demos/compute_quantities_paper.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 from psf_generator.utils.handle_data import save_as_npy
@@ -7,14 +8,14 @@ from src.psf_generator.propagators import *
 if __name__ == "__main__":
     n_pix_pupil = 127
     n_pix_psf = 256
-    na = 1.4
+    na = 1.3
     wavelength = 600
     fov = 2000
     defocus = 4000
     n_defocus = 256
-    e0x = 1
-    e0y = 1j
-    mask = 'vortex'
+    e0x = math.sqrt(2) / 2
+    e0y = e0x * 1j
+    mask = None
     zernike_coefficients = None
 
     kwargs = {
@@ -32,24 +33,22 @@ if __name__ == "__main__":
     }
 
     # define base file path
-    exp_name = 'donut_high_na_e0_1_1j'
+    exp_name = 'pure_gaussian_high_na_e0_1_1j'
     base_data_path = os.path.join('results', 'data', 'fields', exp_name)
     os.makedirs(base_data_path, exist_ok=True)
 
     # define propagators
     propagators = [
         ScalarCartesianPropagator(special_phase_mask=mask, **kwargs),
-        # ScalarSphericalPropagator(**kwargs),
         VectorialCartesianPropagator(e0x=e0x, e0y=e0y, special_phase_mask=mask, **kwargs),
-        # VectorialSphericalPropagator(e0x=e0x, e0y=e0y, **kwargs),
     ]
 
     for propagator in propagators:
         name = propagator.get_name()
-        pupil = propagator.get_pupil()
         field = propagator.compute_focus_field()
+        pupil = propagator.get_pupil()
         # save data as .npy
-        for data, data_name in zip([pupil, field], ['pupil', 'psf']):
+        for data, data_name in zip([field, pupil], ['psf', 'pupil']):
             filepath = os.path.join(base_data_path, f'{name}_{data_name}.npy')
             save_as_npy(filepath, data)
         # save parameters
